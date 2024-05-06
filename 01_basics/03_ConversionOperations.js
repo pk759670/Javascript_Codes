fb5b1cd4895bd03fb7de0e765f521f3c2017defe
PRINCE KUMAR <132484168+pk759670@users.noreply.github.com>
2024-05-06 15:21:31 +0000

    Operations discussed

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,43 @@
+let score=33
+
+//console.log(typeof score);
+// console.log(typeof (score));
+
+let score1="33"
+// console.log(typeof score1);
+
+let valueInNumber=Number(score1)
+// console.log(valueInNumber);
+
+let score2="33abc"
+let vInNumber=Number(score2)
+//console.log(vInNumber);
+
+let score3=null
+let vInNumber1=Number(score3)
+
+//console.log(vInNumber1);
+//console.log(typeof vInNumber1); 
+
+let score4= undefined
+let vInNumber2=Number(score4)
+
+// console.log(vInNumber2);
+// console.log(typeof vInNumber2);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+// null => 0;undefined => NaN(Not A Number)
+
+let name="34"
+
+console.log([typeof String(name), String(name)]);
+
+let isloggedIn = 1
+let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
