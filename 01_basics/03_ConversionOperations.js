876a0773df322cb120fe6095d906c47a813ba009
PRINCE KUMAR <132484168+pk759670@users.noreply.github.com>
2024-05-08 06:21:44 +0000

    ConversionOperations Commit

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -77,4 +77,16 @@ num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
 ++gameCounter;
-console.log(gameCounter);
+//console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
