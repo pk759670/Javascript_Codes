323d38c3b7e83a7dcd6e550be4046084421d820c
PRINCE KUMAR <132484168+pk759670@users.noreply.github.com>
2024-05-10 05:05:06 +0000

    ConversionOperations commit

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -68,8 +68,8 @@ let str3 = str1 + str2
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+ //console.log(+true); // give 1
+ //console.log(+""); // give 0
 
 let num1, num2, num3
 
